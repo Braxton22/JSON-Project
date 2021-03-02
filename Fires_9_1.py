72b1e3bdb59a3b724ee2f9ba0cc83141f80620a7
Braxton Bobo <Braxton_bobo@baylor.edu>
2021-03-01 18:50:16 -0600

    Final Submit

---
+++ Fires_9_1.py
@@ -0,0 +1,41 @@
+import json
+from plotly.graph_objects import Scattergeo, Layout
+from plotly import offline
+import plotly.express as px
+
+infile = open("US_fires_9_1.json", "r")
+
+Fire_data = json.load(infile)
+
+Bright_Fires = [fire for fire in Fire_data if fire["brightness"] >= 450]
+
+brightnesses, lats, lons = [], [], []
+
+for fire in Bright_Fires:
+    brightnesses.append(fire["brightness"])
+    lons.append(fire["longitude"])
+    lats.append(fire["latitude"])
+
+data = [
+    {
+        "type": "scattergeo",
+        "lon": lons,
+        "lat": lats,
+        "marker": {
+            "size": [0.035 * brightness for brightness in brightnesses],
+            "color": brightnesses,
+            "colorscale": "Viridis",
+            "reversescale": True,
+            "colorbar": {"title": "Brightness"},
+        },
+    }
+]
+
+Graph_legend = {"title": "Brightness"}
+
+my_layout = Layout(
+    title="US Fires - 9/1/2020 through 9/13/2020",
+    legend=Graph_legend,
+)
+fig = {"data": data, "layout": my_layout}
+offline.plot(fig, filename="California_Fires.html")
---
+++ explore_json_3.py
@@ -0,0 +1,55 @@
+import json
+
+infile = open("eq_data_1_day_m1.json", "r")
+outfile = open("readable_earthquake_data.json", "w")
+
+# the json.load() function converst the data into a format python can work with: in this case, a giant dictionary
+eq_data = json.load(infile)
+
+json.dump(eq_data, outfile, indent=4)
+
+# print(eq_data["features"][0]["properties"]["mag"])
+
+list_of_eqs = eq_data["features"]
+mags = []
+mags, lons, lats, hover_values = [], [], [], []
+
+for eq in list_of_eqs:
+    mag = eq["properties"]["mag"]
+    lon = eq["geometry"]["coordinates"][0]
+    lat = eq["geometry"]["coordinates"][1]
+    hover_value = eq["properties"]["place"]
+    mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
+    hover_values.append(hover_value)
+
+print(mags[:10])
+print(lons[:10])
+print(lats[:10])
+
+from plotly.graph_objects import Scattergeo, Layout
+from plotly import offline
+
+data = [
+    {
+        "type": "scattergeo",
+        "lon": lons,
+        "lat": lats,
+        "text": hover_values,
+        "marker": {
+            "size": [5 * mag for mag in mags],
+            "color": mags,
+            "colorscale": "Viridis",
+            "reversescale": True,
+            "colorbar": {"title": "Magnitude"},
+        },
+    }
+]
+
+
+my_layout = Layout(title="Global earthquakes")
+
+fig = {"data": data, "layout": my_layout}
+
+offline.plot(fig, filename="global_earthquakes.html")
